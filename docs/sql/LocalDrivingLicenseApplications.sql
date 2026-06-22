dff705dca94e0b8a2eadd6b02f75ff7b07ca2550
anasemadanas <anasemadanas1@gmail.com>
2026-06-22 20:48:20 +0300

    update schema

--- docs/sql/LocalDrivingLicenseApplications.sql
+++
@@ -1,9 +0,0 @@
-use DVLD;
-
-
-INSERT INTO LocalDrivingLicenseApplications (LocalDrivingLicenseApplicationID, ApplicationID, LicenseClassID) VALUES
-(30, 52, 3),
-(31, 65, 3),
-(32, 68, 7);
-
-select * from localdrivinglicenseapplications;
--- docs/sql/users.sql
+++
@@ -1,9 +0,0 @@
-use dvld;
-
-insert into users
-values 
-(1, 1, 'Msaqer77', 1234, 1),
-(15, 1025, 'user4', 1234, 1);
-go
-
-select * from users;
